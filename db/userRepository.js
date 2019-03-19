2e5307f34c36a0e41e05c58fd7c7dd249472894b
Sergey Kobyakov <snkobyakov@yandex.ru>
2019-03-19 13:20:35 +0500

    Implemented user deletion

--- db/userRepository.js
+++ db/userRepository.js
@@ -1,20 +1,56 @@
 const User = require("./model/user").User;
 const ConnectionManager = require("./ConnectionManager");
 
-let getUsers = function() {
-    console.log("Not implemented");    
+let getUsers = async () => {
+    let repository = await ConnectionManager.getRepository(User);
+    var users = await repository.find();
+    console.log("Get users completed");
+    return users;    
 }
 
-let saveUsers = function(users) {
-    console.log("Not implemented");
+let updateUser = async(user) => {
+    let repository = await ConnectionManager.getRepository(User);    
+    let userDb = await repository.findOne(user.id);
+
+    if (!userDb) return false;
+
+    userDb.name = user.name;
+    userDb.age = user.age;
+
+    await repository.save(userDb);    
+    console.log(`User updated:${JSON.stringify(userDb)}`);
+
+    return true;
+}
+
+let getUser = async(userId) => {
+    let repository = await ConnectionManager.getRepository(User);
+    let user = await repository.findOne(userId);    
+    console.log(`Get user completed:${JSON.stringify(user)}`);
+    return user;
 }
 
 let addUser = async user => {        
-    let userRepository = await ConnectionManager.getRepository(User);
-    await userRepository.save(user);
+    let repository = await ConnectionManager.getRepository(User);
+    await repository.save(user);
     console.log(`Add user completed:${JSON.stringify(user)}`);
 } 
 
-module.exports.addUser = addUser;
-module.exports.getUsers = getUsers;
-module.exports.saveUsers = saveUsers;
+let remove = async userId => {
+    let repository = await ConnectionManager.getRepository(User);
+    let userDb = await repository.findOne(userId);
+    
+    if (!userDb) return false;
+
+    await repository.remove(userDb);    
+    console.log(`Remove user completed:${JSON.stringify(userDb)}`);
+    return true;
+}
+
+module.exports = {
+    addUser: addUser,
+    getUsers: getUsers,
+    getUser:  getUser,
+    updateUser: updateUser,
+    remove: remove
+}
--- src/users.js
+++ src/users.js
@@ -1,8 +1,8 @@
 const userRepository = require("../db/userRepository.js")
 const User = require("../db/model/user.js").User;
 
-let getAll = function (req, res) {
-    let users = repository.getUsers();
+let getAll = async (req, res) => {
+    let users = await userRepository.getUsers();
     res.send(users);
 };
 
@@ -17,58 +17,24 @@ let add = async (req, res) => {
     res.send(user);
 };
 
-let update = function (req, res) {
+let update = async (req, res) => {
     if (!req.body) return res.sendStatus(400);
 
-    let userId = req.body.id;
-    let userName = req.body.name;
-    let userAge = req.body.age;
+    if (!req.body) return res.status(400).send("User not defined");
 
-    if (!userId) return res.status(400).send("UserId not defined");
+    if (!await userRepository.updateUser(req.body))
+        return res.status(400).send("User not found");    
 
-    let users = repository.getUsers();
-    let user;
-
-    for (let i = 0; i < users.length; i++)
-    {
-        if (users[i].id == userId)
-        {
-            user = users[i];
-            break;
-        }
-    }
-
-    if (!user) return res.status(400).send("User not found");
-
-    user.name = userName;
-    user.age = userAge;
-
-    repository.saveUsers(users);    
     res.send(user);
 };
 
-let remove = function (req, res) {
-    if (!req.body) return res.sendStatus(400);
+let remove = async (req, res) => {
+    if (!req.body.id) return res.sendStatus(400);
+    
+    let removed = await userRepository.remove(req.body.id);
+    if (!removed)
+        return res.status(400).send("User not found");    
 
-    let userId = req.body.id;
-
-    if (!userId) return res.status(400).send("UserId not defined");
-
-    let users = repository.getUsers();
-    let index = -1;
-    for (let i = 0; i < users.length; i++)
-    {
-        if (users[i].id == userId)
-        {
-            index = i;
-            break;
-        }
-    }
-
-    if (index == -1) return res.status(400).send("User not found");
-
-    users.splice(index, 1)[0];
-    repository.saveUsers(users);    
     res.sendStatus(200);
 };
 
